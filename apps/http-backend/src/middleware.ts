cb9267d81a8f2b6822d546d0ae7eaeb0c021fb6e
Mounish Vatti <mounishvatti2002@gmail.com>
2025-01-21 22:04:46 +0530

    type error fix

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -2,15 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
+interface CustomRequest extends Request {
+    userId?: string;
+}
 
-export function middleware(req: Request, res: Response, next: NextFunction) {
+export function middleware(req: CustomRequest, res: Response, next: NextFunction) {
     const token = req.headers["authorization"] ?? "";
 
     const decoded = jwt.verify(token, JWT_SECRET);
 
     if (decoded) {
-        // @ts-ignore: TODO: Fix this
-        req.userId = decoded.userId;
+        req.userId = (decoded as jwt.JwtPayload).userId;
         next();
     } else {
         res.status(403).json({
